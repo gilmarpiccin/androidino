b22babafd41c976f884b15fed5fea4aab5214ed1
gilmarpiccin <gilmarpiccin@85969506-206e-e1ca-e2cb-2ee4545cd2cd>
2012-09-19 23:17:30 +0000

    Refatoração (padronização de variaveis e estruturação do fonte)

---
+++ WebServer/ArquivoSD.ino
@@ -0,0 +1,63 @@
+/*
+Serviço Cartão SD
+*/
+
+#include <SD.h>
+
+void iniciaSD(){
+  pinMode(53, OUTPUT);//no Mega porta 53 no outros 10
+  if (!SD.begin(4)) 
+    Serial.println("Inicialização SD Falhou!");
+  else  
+    Serial.println("SD Inincializado com exito.");
+}
+
+String gravaArquivoSD(char cArquivo[],String sTexto){
+  File flFile;
+  iniciaSD();
+  if (!SD.exists(cArquivo)){
+    Serial.println("Não existe arquivo.");
+    return"Não Existe Arquivo.";
+  }
+  flFile = SD.open(cArquivo, FILE_WRITE);
+  
+  if (flFile) {
+    Serial.print("Escrevendo no arquivo: ");
+    Serial.print(cArquivo);
+    flFile.print(sTexto);
+    flFile.close();
+    Serial.println("Gravado com Sucesso.");
+    return "Garavado com Sucesso.";
+  } else {
+    Serial.println("Erro ao Abrir: "  );
+    Serial.print(cArquivo);
+    return "Erro ao Abrir.";
+  }
+}
+
+String lerArquivoSD (char cArquivo[]){
+  iniciaSD();
+  if (!SD.exists(cArquivo)){
+    Serial.println("não existe arquivo");
+    return"";
+  }
+  
+  Serial.println("Lendo Arquivo do SD");
+  File flFile = SD.open(cArquivo);
+  String sTexto;
+  if (flFile) {
+    while (flFile.available()) {
+      char cAux = flFile.read();
+      Serial.write(cAux);    
+      sTexto.concat(cAux);
+    }    
+    Serial.println("\n Fechando o arquivo");
+    Serial.println(sTexto);
+    flFile.close();
+  } else {
+    Serial.println('Erro ao Abrir' + cArquivo);
+    sTexto = "";
+  }
+  return sTexto;
+}
+
---
+++ WebServer/Ethernet.ino
@@ -0,0 +1,28 @@
+/**
+  Serviço Ethernet
+  Recebe por o resultado da função maintain() que renova o DHCP
+**/
+ String trocaDHCP(byte byDHCP){
+   String sRetorno;
+   switch (byDHCP){
+   case 0:
+     sRetorno = "Nada ocorreu.";
+     break;
+   case 1:
+     sRetorno = "Renovação falhou.";
+     break;
+   case 2:
+     sRetorno = "Renovação com Sucesso.";
+     break;
+   case 3:
+     sRetorno = "Falha ao Realigar.";
+     break;
+   case 4:
+     sRetorno = "Sucesso ao Religar";
+     break;
+   default:
+      sRetorno = "";
+   } 
+   
+   return sRetorno;
+ }
