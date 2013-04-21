212ff392afb36f42d2e4d4b33371e24dbfd72680
gilmarpiccin <gilmarpiccin@85969506-206e-e1ca-e2cb-2ee4545cd2cd>
2013-04-21 03:43:00 +0000

    Add funcionalidade Tempo.ino

---
+++ WebServer/Tempo.ino
@@ -0,0 +1,18 @@
+
+
+String Tempo(){
+  DateTime now = RTC.now();
+  String sHora = "";
+  sHora.concat(now.day());
+  sHora.concat('/');
+  sHora.concat(now.month());
+  sHora.concat('/');
+  sHora.concat(now.year());
+  sHora.concat(' ');
+  sHora.concat(now.hour());
+  sHora.concat(':');
+  sHora.concat(now.minute());
+  sHora.concat(':');
+  sHora.concat(now.second());
+ return sHora;   
+}
